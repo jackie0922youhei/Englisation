1b5e2d53ac3cf64052722191a6c411ec382e35b0
jackie0922youhei <jackie0922youhei@gmail.com>
2020-12-02 08:06:15 +0000

    making controllers

---
+++ app/controllers/customers/reviews_controller.rb
@@ -0,0 +1,13 @@
+class Customers::ReviewsController < ApplicationController
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ test/controllers/customers/comments_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class Customers::CommentsControllerTest < ActionDispatch::IntegrationTest
+  test "should get create" do
+    get customers_comments_create_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get customers_comments_destroy_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get customers_comments_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get customers_comments_update_url
+    assert_response :success
+  end
+
+end
