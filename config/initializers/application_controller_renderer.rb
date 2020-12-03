4b3be15330406295371cb84db1985b415852e006
jackie0922youhei <jackie0922youhei@gmail.com>
2020-12-03 07:56:19 +0000

    finished until withdraw

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5c4bb940cdff31c1ed95793e51bac1d53ef4828dfdf4b9524fbe9fbcbb843984a01337689ab09435e69bcd63e1aaefef79d76842da7374dce603f9b4caa0cfe8'
