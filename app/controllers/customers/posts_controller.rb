1f61cdb4378fd5472dbca0f970970d2d711df87f
jackie0922youhei <jackie0922youhei@gmail.com>
2020-12-03 08:52:33 +0000

    still creating post index page

--- app/controllers/customers/posts_controller.rb
+++ app/controllers/customers/posts_controller.rb
@@ -1,11 +1,22 @@
 class Customers::PostsController < ApplicationController
   def index
-  end
-
-  def new
+    @posts = Post.all.order(created_at: :desc)
+    @customer = current_customer
+    @teachers = Customer.all.where(is_teacher: true)
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.customer_id = current_customer.id
+    if @post.save
+      redirect_to posts_path
+    else
+      @posts = Post.all.order(created_at: :desc)
+      @customer = current_customer
+    @teachers = Customer.all.where(is_teacher: true)
+      render :index
+    end
   end
 
   def show
@@ -19,4 +30,10 @@ class Customers::PostsController < ApplicationController
 
   def destroy
   end
+
+  private
+  def post_params
+    params.require(:post).permit(:body, :customer_id, :reference, :tag_list_id)
+  end
+
 end
