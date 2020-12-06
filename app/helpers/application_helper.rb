ee0dcd6f44484a352313a0681e04e0438591a9cf
jackie0922youhei <jackie0922youhei@gmail.com>
2020-12-06 02:22:52 +0000

    changes a lot

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  
+  def notification_form(notification)
+    @visiter = notification.visiter
+    @comment = nil
+    your_post = link_to 'あなたの投稿', post_path(notification), style:"font-weight: bold;"
+    @visiter_comment = notification.comment_id
+    #notification.actionがfollowかlikeかcommentか
+    case notification.action
+      when "follow" then
+        tag.a(notification.visiter.name, href:mypage_path(@visiter), style:"font-weight: bold;")+"があなたをフォローしました"
+      when "favorite" then
+        tag.a(notification.visiter.name, href:mypage_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:post_path(notification.post_id), style:"font-weight: bold;")+"にいいねしました"
+      when "comment" then
+        @comment = Comment.find_by(id: @visiter_comment)&.content
+        tag.a(@visiter.name, href:mypage_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:post_path(notification.post_id), style:"font-weight: bold;")+"にコメントしました"
+    end
+  end
+  
 end
