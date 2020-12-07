6a2bbd258d1c851e439aabd037fb73be50e31ec9
jackie0922youhei <jackie0922youhei@gmail.com>
2020-12-07 08:57:10 +0000

    created follow remote function

--- app/controllers/customers/relationships_controller.rb
+++ app/controllers/customers/relationships_controller.rb
@@ -6,12 +6,13 @@ class Customers::RelationshipsController < ApplicationController
     #通知の作成
     followed_customer = Customer.find(params[:customer_id])
     followed_customer.create_notification_follow!(current_customer)
-    redirect_to mypage_path(current_customer.id)
+    render :create
   end
 
   def destroy
     follow = current_customer.active_relationships.find_by(follower_id: params[:customer_id])
     follow.destroy
-    redirect_to root_path
+    @customer = current_customer
+    render :destroy
   end
 end
