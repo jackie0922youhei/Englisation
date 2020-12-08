c2a961a621176efa31e37fd79fa74f4a9a060cde
jackie0922youhei <jackie0922youhei@gmail.com>
2020-12-08 00:14:13 +0000

    finished favorite remote function

--- app/controllers/customers/relationships_controller.rb
+++ app/controllers/customers/relationships_controller.rb
@@ -2,7 +2,7 @@ class Customers::RelationshipsController < ApplicationController
   def create
     follow = current_customer.active_relationships.build(follower_id: params[:customer_id])
     follow.save
-    @customer = current_customer
+    @customer = Customer.find(params[:customer_id])
     #通知の作成
     followed_customer = Customer.find(params[:customer_id])
     followed_customer.create_notification_follow!(current_customer)
@@ -12,7 +12,7 @@ class Customers::RelationshipsController < ApplicationController
   def destroy
     follow = current_customer.active_relationships.find_by(follower_id: params[:customer_id])
     follow.destroy
-    @customer = current_customer
+    @customer = Customer.find(params[:customer_id])
     render :destroy
   end
 end
