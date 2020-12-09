fd6a30ddd1f9fd75b85e823e63425dd573ce02f9
jackie0922youhei <jackie0922youhei@gmail.com>
2020-12-09 04:13:29 +0000

    created admin's customer index

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -1,9 +1,11 @@
 class Admins::CustomersController < ApplicationController
   
   def index
+    @customers = Customer.all
   end
   
   def show
+    @customer = Customer.find(params[:id])
   end
   
   def edit
