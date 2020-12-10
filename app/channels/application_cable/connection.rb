80d45787552fd8321c67cea40b9dee9714691980
jackie0922youhei <jackie0922youhei@gmail.com>
2020-12-10 00:35:41 +0000

    can't send DM but don't know why

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,23 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    # ApplicationCable::Connectionクラスを使って、認証情報を定義
+    # identified_byはコネクションを識別するキーとなるもの
+    identified_by :current_customer
+
+    def connect
+      # connectメソッドはコネクションの接続時に呼ばれるメソッド
+      self.current_customer = find_verified_customer
+    end
+
+    protected
+    def find_verified_customer
+      # コネクションの識別キーとして、ログイン時に設定したCookieからcustomer_idを取り出している
+      if verified_customer = Customer.find_by(id: cookies.signed['customer.id'])
+        verified_customer
+      else
+        reject_unauthorized_connection
+      end
+    end
+
   end
 end
---
+++ app/jobs/direct_message_broadcast_job.rb
@@ -0,0 +1,17 @@
+class DirectMessageBroadcastJob < ApplicationJob
+  queue_as :default
+
+  # performメソッドでブロードキャストを実行
+  def perform(direct_message)
+    ActionCable.server.broadcast "room_channel_#{direct_message.room_id}", direct_message: render_direct_message(direct_message)
+    # Do something later
+  end
+
+  private
+
+  def render_direct_message(direct_message)
+    # direct_messageに単純な文字列ではなく、direct_messages/direct_messagパーシャルのHTMLを返す
+    ApplicationController.renderer.render partial: 'direct_messages/direct_message', locals: { direct_message: direct_message }
+  end
+
+end
