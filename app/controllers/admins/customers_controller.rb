401af7461d15fd6ecec12b9b096d1852d4d146de
jackie0922youhei <jackie0922youhei@gmail.com>
2020-12-16 08:47:13 +0000

    fixed ranking layout

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -6,6 +6,7 @@ class Admins::CustomersController < ApplicationController
 
   def show
     @customer = Customer.find(params[:id])
+    @posts = @customer.posts.all.order(created_at: :desc)
   end
 
   def edit
