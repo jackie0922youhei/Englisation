bafd6b56a8203729c4c51551796c15c73865c50c
jackie0922youhei <jackie0922youhei@gmail.com>
2021-01-10 10:34:19 +0000

    finish user search

--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,6 @@ Rails.application.routes.draw do
 
   get 'homes/about'
   root 'customers/posts#index'
-  get 'customers/searches/search'
   get '/search', to: 'customers/searches#search'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
