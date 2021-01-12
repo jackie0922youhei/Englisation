156e431ae6baa2aedf6fd4d0ef0be8cb203cb614
jackie0922youhei <jackie0922youhei@gmail.com>
2021-01-12 12:01:27 +0000

    fixed reverse scroll

--- app/assets/javascripts/reverse_scroll.js
+++ app/assets/javascripts/reverse_scroll.js
@@ -1,7 +1,6 @@
 /* global $*/
 $(function() {
   var nextLoadPage = 2;
-  console.log('これが出てくるかな')
   $(".direct_messages").scrollTop(1);
   $(".direct_messages").scroll(function() {
     // $(".chat").scrollTop() == 0: 指定した要素のスクロール位置が0かどうか
@@ -9,7 +8,7 @@ $(function() {
     // $(".message_view").find("nav.pagination span.last a"): ページネーションリンクの最後のページを指すリンクを指定
     // .prop("search"): リンクのURLパラメータ(eg.?page=2)を取得
     // .match(/[0-9]/), 10): 正規表現で数字にマッチするもの(=ページ番号(e.g. 2))を取得
-    if (($(".direct_messages").scrollTop() == 0) && (nextLoadPage <= parseInt($(".pagination").find("li:last a").prop('search').match(/[0-9]/), 10))){
+    if (($(".direct_messages").scrollTop() == 0) && (nextLoadPage <= parseInt($('.direct_messages').data('total-pages'), 10))){
       $.ajax({
         url: $("ul.pagination").find("a[rel=next]").prop('search').replace(/[0-9]/, nextLoadPage)
       }).done(function(data) {
